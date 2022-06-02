814ba0dec1b88d3e236102d302139c661f83d473
neenteen00 <106771947+neenteen00@users.noreply.github.com>
2022-06-03 01:59:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi cậu, cô bé m50",
+    introTitle: 'Lương neeeeee',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    title: 'Trong tôi đã đầy một tim rồi 🥰',
+    desc: 'Còn cậu thì sao ? ',
+    btnYes: 'Tớ thích lắm <33',
+    btnNo: 'Khum nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích mình <3',
+    btnReply: 'Gửi cho cậu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Hẹn cậu một ngày đẹp trời cùng đi chơi Hà Nội.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/ducluong1501' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
